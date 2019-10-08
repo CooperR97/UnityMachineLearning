77240dfd168da48d3efaa2f28df77f43d76ffe45
Cooper Redfern <coopr97@gmail.com>
2019-10-07 22:11:44 -0500

    Added first custom training environment

---
+++ squareCharML/Assets/customGameAssets/Scripts/PlayerMovement.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed;
+    public float jumpForce;
+    private float moveInput;
+
+    private Rigidbody2D rb2d;
+
+    private bool facingRight = true;
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGrounded;
+
+    private int extraJumps;
+    public int extraJumpsValue;
+    public float force;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGrounded);
+        moveInput = Input.GetAxis("Horizontal");
+        //rb2d.AddForce(new Vector2(moveInput * force, 0f));
+        rb2d.velocity = new Vector2(moveInput * speed, rb2d.velocity.y);
+
+        if (facingRight == false && moveInput > 0)
+        {
+            Flip();
+        }
+        else if (facingRight == true && moveInput < 0)
+        {
+            Flip();
+        }
+    }
+
+    private void Update()
+    {
+
+        if (isGrounded)
+        {
+            extraJumps = extraJumpsValue;
+        }
+        if(Input.GetKeyDown(KeyCode.UpArrow) && extraJumps > 0)
+        {
+            rb2d.velocity = Vector2.up * jumpForce;
+            extraJumps--;
+        } else if(Input.GetKeyDown(KeyCode.UpArrow) && extraJumps == 0 && isGrounded == true)
+        {
+            rb2d.velocity = Vector2.up * jumpForce;
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        transform.localScale = new Vector3(-transform.localScale.x, 1, 1);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("goal"))
+        {
+            Debug.Log("You hit the goal");
+        }
+    }
+}
---
+++ squareCharML/Assets/customGameAssets/Scripts/SquareCharAgent.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+
+public class SquareCharAgent : Agent
+{
+
+    Rigidbody2D rb2d;
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    public Transform Target;
+
+    public override void AgentReset()
+    {
+        if(this.transform.position.y < 0)
+        {
+            //if the agent falls off the platform, 0 the position
+            this.rb2d.angularVelocity = 0f;
+            this.rb2d.velocity = Vector2.zero;
+            this.transform.position = new Vector3(0, 2f, 0);
+        }
+
+        // Move the target to a new position(tutorial)
+        // For our example we are keeping it real simple and not moving the target
+        Target.position = new Vector3(Random.value * 8, Random.value * 8, 0);
+    }
+
+    public override void CollectObservations()
+    {
+        // target and agent positions
+        AddVectorObs(Target.position);
+        AddVectorObs(this.transform.position);
+
+        // Agent velocity
+        AddVectorObs(rb2d.velocity.x);
+        AddVectorObs(rb2d.velocity.y);
+
+        
+    }
+
+    public float speed = 30f;
+
+    public override void AgentAction(float[] vectorAction, string textAction)
+    {
+        //Actions, size = 2
+        Vector2 controlSignal = Vector2.zero;
+        controlSignal.x = vectorAction[0];
+        controlSignal.y = vectorAction[1];
+        rb2d.AddForce(controlSignal * speed);
+
+        //Rewards
+        float distanceToTarget = Vector2.Distance(this.transform.position, Target.position);
+
+        //reached the target
+        if (distanceToTarget < 1.5f)
+        {
+            AddReward(1.0f);
+            Done();
+            
+        }
+        //reached the target
+        if (distanceToTarget < 5f)
+        {
+            AddReward(0.1f);
+          
+
+        }
+
+        //Fell off the platform
+        if (this.transform.position.y < 0)
+        {
+            AddReward(-0.1f);
+            Done();
+            
+        }
+        //Fell off the platform
+        if (this.transform.position.y > 8f)
+        {
+            Done();
+            
+        }
+        //Fell off the platform
+        if (this.transform.position.x > 30f)
+        {
+            Done();
+            
+        }
+        //Fell off the platform
+        if (this.transform.position.x < -30f)
+        {
+            Done();
+            
+        }
+
+    }
+
+}
